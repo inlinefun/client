46c31ba11a2356d457a18f8c33f20b94b6da3050
inlinefun <inlinefun@proton.me>
2025-09-03 13:58:35 +0530

    feat: Use a second dispatcher + log cleanup

--- src/main/kotlin/blend/Blend.kt
+++ src/main/kotlin/blend/Blend.kt
@@ -1,22 +1,52 @@
 package blend
 
 import blend.api.KtorServer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import net.minecraft.client.MinecraftClient
 import org.slf4j.LoggerFactory
+import java.io.File
+import java.util.concurrent.Executors
 
 object Blend {
 
     const val NAME = "Blend"
     const val VERSION = "6.0"
     val logger = LoggerFactory.getLogger(NAME)!!
+    val folder = File(MinecraftClient.getInstance().runDirectory, NAME.lowercase()).also { folder ->
+        if (!folder.exists())
+            folder.mkdir()
+    }
+
+    private val dispatcher = Executors.newSingleThreadExecutor { task ->
+        Thread(task, "$NAME Client")
+    }.asCoroutineDispatcher()
+    private val scope = CoroutineScope(context = SupervisorJob() + dispatcher)
 
     fun initialize() {
         val preInit = System.currentTimeMillis()
-        KtorServer.start()
+
+        runAsync {
+            KtorServer.start()
+            logger.info("Test")
+        }
+
         logger.info("Initialized $NAME v$VERSION in ${System.currentTimeMillis() - preInit}ms")
     }
 
     fun shutdown() {
+        scope.cancel(message = "Client Shutdown")
+        dispatcher.close()
         KtorServer.stop()
+    }
+
+    fun runAsync(
+        block: suspend CoroutineScope.() -> Unit
+    ) = scope.launch {
+        block()
     }
 
 }
--- src/main/kotlin/blend/api/KtorServer.kt
+++ src/main/kotlin/blend/api/KtorServer.kt
@@ -1,25 +1,30 @@
 package blend.api
 
+import io.ktor.server.engine.applicationEnvironment
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.slf4j.LoggerFactory
 
 object KtorServer {
 
-    val server = embeddedServer(
+    private val server = embeddedServer(
+        configure = {
+            connector {
+                host = "0.0.0.0"
+                port = 5555
+            }
+        },
         factory = Netty,
-        port = 5555,
-        host = "0.0.0.0",
         module = {
             configureRouting()
+        },
+        environment = applicationEnvironment {
+            log = LoggerFactory.getLogger("Ktor")
         }
     )
 
-    fun start() {
-        server.start(wait = false)
-    }
-
-    fun stop() {
-        server.stop()
-    }
+    fun start() = server.start(wait = false)
+    fun stop() = server.stop()
 
 }
